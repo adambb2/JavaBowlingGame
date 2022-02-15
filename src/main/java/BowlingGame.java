0f7b394b44c43b0e3be2cec2d670eefa8169accd
adam.b.bell <adam.b.bell@cgi.com>
2022-02-14 19:18:58 -0600

    create Frame class and basic BowlingGame methods

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,1 +1,65 @@
- 
+import java.util.Hashtable;
+import java.util.Stack;
+
+public class BowlingGame {
+	
+	private Integer maxFrameCount = 10;
+	
+	private Integer maxPinCount = 10;
+	
+	private Integer strikePinCount = 10;
+	
+	private Hashtable<Integer, String> frameDictionary = new Hashtable<Integer, String>();
+	
+	
+	
+	
+	/**
+	 * default constructor
+	 */
+	public BowlingGame() {
+	}
+
+	/**
+	 * Method that updates the bowling game frame history with the number of pins that are knocked down with each roll. This method must be called
+	 * in the sequential order that the rolls occured.
+	 * @param pins
+	 * @return void
+	 */
+	public void roll(Integer pins) {
+		try {
+			validateRoll(pins);
+			if(pins == strikePinCount) {
+				System.out.println("Rolled a STRIKE! Moving to next frame.");
+			}	
+		}
+		catch(IllegalStateException ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	/**
+	 * Method that calculates the score of the bowling game. Can only be called at the end of the 10 frame game.
+	 * @return score
+	 */
+	public int score() {
+		return 1;
+	}
+	
+	
+	/**
+	 * Method that validates that the number of pins knocked down by the roll is not outside what is possible for a roll in the current frame
+	 * @param pins
+	 */
+	private void validateRoll(Integer pins) {
+		if(pins > maxPinCount) {
+			throw new IllegalStateException("Pin count exceeds pins on the lane");
+		}
+		if(pins < 0) {
+			throw new IllegalStateException("Negative roll is invalid");
+		}
+	}
+	
+	
+	
+}
---
+++ src/main/java/Frame.java
@@ -0,0 +1,65 @@
+
+public class Frame {
+	
+	private Integer firstBallPinCount = 0;
+	
+	private Integer secondBallPinCount = 0; //second ball only occurs if a strike was NOT rolled on the first ball..  OR if it is the 10th frame
+	
+	private Integer fillBallPinCount = 0; //a fill ball is given if a strike of spare occurs on the 10th frame
+
+	/**
+	 * @return the firstBallPinCount
+	 */
+	public Integer getFirstBallPinCount() {
+		return firstBallPinCount;
+	}
+
+	/**
+	 * @param firstBallPinCount the firstBallPinCount to set
+	 */
+	public void setFirstBallPinCount(Integer firstBallPinCount) {
+		this.firstBallPinCount = firstBallPinCount;
+	}
+
+	/**
+	 * @return the secondBallPinCount
+	 */
+	public Integer getSecondBallPinCount() {
+		return secondBallPinCount;
+	}
+
+	/**
+	 * @param secondBallPinCount the secondBallPinCount to set
+	 */
+	public void setSecondBallPinCount(Integer secondBallPinCount) {
+		this.secondBallPinCount = secondBallPinCount;
+	}
+
+	/**
+	 * @return the fillBallPinCount
+	 */
+	public Integer getFillBallPinCount() {
+		return fillBallPinCount;
+	}
+
+	/**
+	 * @param fillBallPinCount the fillBallPinCount to set
+	 */
+	public void setFillBallPinCount(Integer fillBallPinCount) {
+		this.fillBallPinCount = fillBallPinCount;
+	}
+
+	/**
+	 * @param firstBallPinCount
+	 * @param secondBallPinCount
+	 * @param fillBallPinCount
+	 */
+	public Frame(Integer firstBallPinCount, Integer secondBallPinCount, Integer fillBallPinCount) {
+		this.firstBallPinCount = firstBallPinCount;
+		this.secondBallPinCount = secondBallPinCount;
+		this.fillBallPinCount = fillBallPinCount;
+	}
+	
+
+	
+}
